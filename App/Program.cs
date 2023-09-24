2cc0ff47a4e6f6160ad03063fb5027edf4fcc9ba
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-09-24 21:13:48 +0530

    initail commit

---
+++ App/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let us Display List of Month");
+List<string> listMonth = new List<string>
+{
+    "Jan","feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+};
+foreach (var month in listMonth) 
+{
+    Console.WriteLine(month);
+    Thread.Sleep(500);
+}
